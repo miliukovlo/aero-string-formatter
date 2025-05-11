13ab8f94af44845e1fda64a5c051fafd0be92a1b
ssadown <bibatututu@yandex.ru>
2025-05-11 16:41:47 +0500

    Fix: rename function to correct name

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './floatToString';
-export * from './formatDateToString';
-export * from './formatLocalPhoneNumber';
-export * from './formatNumberToPriceString';
-export * from './getNoun';
-export * from './numberFormatterToPrice';
-export * from './numberFormatterToRoman';
-export * from './priceFormatter';
-//# sourceMappingURL=index.d.ts.map
