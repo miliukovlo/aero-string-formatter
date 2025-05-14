4d5fc66afd979eb7b10afaf76d1e055c461d946d
ssadown <bibatututu@yandex.ru>
2025-05-14 21:35:12 +0500

    Feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production', // или 'development' для разработки
+  entry: './src/index.ts', // путь к вашему файлу index.ts
+  output: {
+    filename: 'bundle.js', // имя выходного файла
+    path: path.resolve(__dirname, 'dist'), // директория для выходного файла
+    libraryTarget: 'umd', // Поддержка CommonJS, AMD и глобальных переменных
+    globalObject: 'this'
+  },
+  optimization: {
+    minimize: true,
+    usedExports: true
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.js'], // расширения файлов, которые нужно обрабатывать
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/, // обрабатываем только .ts файлы
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
